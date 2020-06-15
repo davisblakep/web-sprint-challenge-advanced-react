59c84d3de7071b91d3459b8df696f6571325169d
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-06-14 20:14:00 -0400

    Completed testing for all form fields and success pop-up.  Performed successful sanity check

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -9,4 +9,43 @@ test("form header renders", () => {
     screen.getByText(/checkout form/i)
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    const firstName = screen.getByLabelText("First Name:");
+    const lastName = screen.getByLabelText("Last Name:");
+    const address = screen.getByLabelText("Address:");
+    const city = screen.getByLabelText("City:");
+    const state = screen.getByLabelText("State:");
+    const zip = screen.getByLabelText("Zip:");
+
+
+    fireEvent.change(firstName, { target: { value: "Blake"} });
+    fireEvent.change(lastName, { target: { value: "Davis"} });
+    fireEvent.change(address, { target: { value: "123 Street"} });
+    fireEvent.change(city, { target: { value: "Cheeseburger Town"} });
+    fireEvent.change(state, { target: { value: "OH"} });
+    fireEvent.change(zip, { target: { value: "45678"} });
+
+
+    const submitButton = screen.getByTestId("checkout-button");
+    fireEvent.click(submitButton);
+
+    const successScreen = screen.getByText(/You have ordered some plants! Woo-hoo!/i)
+    const newFirstName = screen.getByText(/blake/i);
+    const newLastName = screen.getByText(/davis/i);
+    const newAddress = screen.getByText(/123 street/i);
+    const newCity = screen.getByText(/cheeseburger town/i);
+    const newState = screen.getByText(/oh/i);
+    const newZip = screen.getByText(/45678/i);
+
+
+    expect(successScreen).toBeInTheDocument();
+    expect(newFirstName).toBeInTheDocument();
+    expect(newLastName).toBeInTheDocument();
+    expect(newAddress).toBeInTheDocument();
+    expect(newCity).toBeInTheDocument();
+    expect(newState).toBeInTheDocument();
+    expect(newZip).toBeInTheDocument();
+
+});
